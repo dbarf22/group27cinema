70ed3969ab6f5039eaae63cc4054703dbef66593
Dylan Barfield <dbarfield4@gmail.com>
2025-09-24 18:40:20 -0400

    MongoDB stuff added

--- cinema_backend/src/main/java/com/group27/cinema_backend/CinemaBackendApplication.java
+++ cinema_backend/src/main/java/com/group27/cinema_backend/CinemaBackendApplication.java
@@ -1,9 +1,9 @@
-package com.group27.cinema_backend.model;
+package com.group27.cinema_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-//import com.group27.cinema_backend.repository.MovieRepository;
+
 
 @SpringBootApplication
 @EnableMongoRepositories
@@ -12,7 +12,6 @@ public class CinemaBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaBackendApplication.class, args);
 	}
-
 
 
 

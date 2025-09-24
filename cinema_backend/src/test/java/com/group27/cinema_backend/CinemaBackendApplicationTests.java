90d0c71f0f2b7f487525077138fbfaedbadd166a
Dylan Barfield <dbarfield4@gmail.com>
2025-09-24 17:16:18 -0400

    Fixed folder structure

---
+++ cinema_backend/src/test/java/com/group27/cinema_backend/CinemaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group27.cinema_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

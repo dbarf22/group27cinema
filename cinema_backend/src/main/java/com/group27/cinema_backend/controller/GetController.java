e9aaf87e1757c12c8e405303039e45cf89141a00
Dylan Barfield <dbarfield4@gmail.com>
2025-09-24 19:38:46 -0400

    Added search by title and search by genre

--- cinema_backend/src/main/java/com/group27/cinema_backend/controller/GetController.java
+++ cinema_backend/src/main/java/com/group27/cinema_backend/controller/GetController.java
@@ -4,6 +4,7 @@ import com.group27.cinema_backend.model.Movie;
 import com.group27.cinema_backend.repository.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,8 +18,18 @@ public class GetController {
         this.movieRepository = movieRepository;
     }
 
+    // leave the paramaters blank to list all movies
+    // if you're searching by title make sure you don't include genre, and vice versa
     @GetMapping("/api/movies")
-    public List<Movie> findAll() {
+    public List<Movie> search(@RequestParam(required = false) String title,
+                              @RequestParam(required = false) String genre) {
+        if (title == null && genre == null) {
+            return movieRepository.findAll();
+        } else if (genre == null){
+            return movieRepository.findByTitleContainingIgnoreCase(title);
+        } else if (title == null) {
+            return movieRepository.findByTitleContainingIgnoreCase(genre);
+        }
         return movieRepository.findAll();
     }
 

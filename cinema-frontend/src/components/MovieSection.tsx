26577fb1ed7833db823afd085b1b25f4755e0602
Dylan Barfield <dbarfield4@gmail.com>
2025-09-29 14:22:44 -0400

    new search?

--- cinema-frontend/src/components/MovieSection.tsx
+++ cinema-frontend/src/components/MovieSection.tsx
@@ -2,42 +2,48 @@
 
 import MovieCard from "./MovieCard";
 import { Movie } from "@/types/movie";
-import {useState} from "react";
+import {useMemo, useState} from "react";
 import {text} from "node:stream/consumers";
 import Form from "next/form";
 
 export default function MovieSection({
   title,
-  movie
+  movies
 }: {
   title: string;
-  movie: Movie;
+  movies: Movie[];
 }) {
 
   const [searchQuery, setSearchQuery] = useState("");
-  const [movies, filteredMovies] = useState<Movie[]>([]);
 
   function movieFilter(title: string) {
       return movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
   }
 
-  const handleSearch = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log("Button hit!");
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearchQuery(e.target.value);
   }
+
+  const filteredMovies = useMemo(() =>{
+      return movieFilter(searchQuery);},
+      [searchQuery]
+  )
 
   return (
     <section className="space-y-10">
-      <div>
-          <button onClick={handleSearch}>click me</button>
-
-      </div>
+      <input
+          type="text"
+          placeholder="Search movies by title..."
+          value={searchQuery}
+          onChange={handleSearchChange}
+          className="p-2 border border-gray-300 rounded-md flex-grow text-sm text-black"
+        />
       <h2 className="text-2xl font-semibold">{title}</h2>
       {movies.length === 0 ? (
         <p className="text-sm text-gray-600">Nothing to show here yet.</p>
       ) : (
         <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-3">
-          {movies.map((m) => (
+          {filteredMovies.map((m) => (
             <MovieCard key={m.id} movie={m} />
           ))}
         </div>

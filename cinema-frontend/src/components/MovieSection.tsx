fb1f8de92c678967f4015a05d9e9603ef8bafa18
Dylan Barfield <dbarfield4@gmail.com>
2025-09-29 11:23:15 -0400

    trying to add search bar

--- cinema-frontend/src/components/MovieSection.tsx
+++ cinema-frontend/src/components/MovieSection.tsx
@@ -1,15 +1,38 @@
+"use client";
+
 import MovieCard from "./MovieCard";
 import { Movie } from "@/types/movie";
+import {useState} from "react";
+import {text} from "node:stream/consumers";
+import Form from "next/form";
 
 export default function MovieSection({
   title,
-  movies,
 }: {
   title: string;
-  movies: Movie[];
 }) {
+
+  const [searchQuery, setSearchQuery] = useState("");
+  const [movies, filteredMovies] = useState<Movie[]>([]);
+
+  function movieFilter(title: string) {
+      return movies.filter(movie => movie.title.toLowerCase().includes(title.toLowerCase()));
+  }
+
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+
+  }
+
   return (
     <section className="space-y-10">
+      <div>
+          <Form className="mx-auto" action={""}>
+            <input name ="query" className="bg-red-100"/>
+            <button className="space-x-3">Submit</button>
+          </Form>
+
+      </div>
       <h2 className="text-2xl font-semibold">{title}</h2>
       {movies.length === 0 ? (
         <p className="text-sm text-gray-600">Nothing to show here yet.</p>

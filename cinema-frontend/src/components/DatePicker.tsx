b2a6bf3f89966a03239605d72c6176da63c56961
ichoi48 <iris.choi@uga.edu>
2025-12-02 16:59:39 -0500

    small ui tweak to datepicker

--- cinema-frontend/src/components/DatePicker.tsx
+++ cinema-frontend/src/components/DatePicker.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
@@ -14,12 +13,21 @@ export default function DatePickerExample({
   setSelectedTime,
 }: DatePickerExampleProps) {
   return (
-    <DatePicker
-      selected={selectedTime}
-      onChange={(d: Date | null) => setSelectedTime(d)}
-      timeInputLabel="Time:"
-      dateFormat="MM/dd/yyyy h:mm aa"
-      showTimeInput
-    />
+     <div className="flex items-center gap-4 max-w-lg">
+      <label className="text-sm font-medium text-gray-700 whitespace-nowrap">
+        Select a date:
+      </label>
+
+      <div className="flex-1 rounded-lg border border-gray-300 bg-white px-4 py-2 shadow-sm hover:border-blue-400 focus-within:border-blue-500 transition">
+
+        <DatePicker
+          selected={selectedTime}
+          onChange={(d: Date | null) => setSelectedTime(d)}
+          timeInputLabel="Time:"
+          dateFormat="MM/dd/yyyy"
+          showTimeInput
+        />
+      </div>
+    </div>
   );
 }

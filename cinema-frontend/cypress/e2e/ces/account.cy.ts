c9d24086fd7509cf90d082c168e4a56772c5ed3f
dbarf22 <dbarfield4@gmail.com>
2025-12-02 18:08:48 -0500

    Added test for accessing pages when logged out.
    Added test for accessing admin pages as a customer.

--- cinema-frontend/cypress/e2e/ces/account.cy.ts
+++ cinema-frontend/cypress/e2e/ces/account.cy.ts
@@ -73,21 +73,68 @@ describe('login system', () => {
         cy.get('[data-testid="expandPaymentMethods"]').click();
         cy.get('[data-testid="expandChangePassword"]').click();
 
-        cy.get('#username').should('have.value','newUsername');
-        cy.get('#firstName').should('have.value','newFirstName');
-        cy.get('#lastName').should('have.value','newLastName');
-        cy.get('#phoneNumber').should('have.value','+1 770 123 8822');
+        cy.get('#username').should('have.value', 'newUsername');
+        cy.get('#firstName').should('have.value', 'newFirstName');
+        cy.get('#lastName').should('have.value', 'newLastName');
+        cy.get('#phoneNumber').should('have.value', '+1 770 123 8822');
 
-        cy.get('#street').should('have.value','newStreet');
-        cy.get('#city').should('have.value','newCity');
-        cy.get('#state').should('have.value','AL');
-        cy.get('#zip').should('have.value','12345');
+        cy.get('#street').should('have.value', 'newStreet');
+        cy.get('#city').should('have.value', 'newCity');
+        cy.get('#state').should('have.value', 'AL');
+        cy.get('#zip').should('have.value', '12345');
 
-        cy.get('#cardExpMonth').should('have.value',"8")
-        cy.get('#cardExpYear').should('have.value',"2025")
-        cy.get('#cardBillingStreet').should('have.value',"1234 New Address")
-        cy.get('#cardBillingCity').should('have.value',"New City")
-        cy.get('#cardBillingState').should('have.value',"AL")
-        cy.get('#cardBillingZip').should('have.value',"12345")
+        cy.get('#cardExpMonth').should('have.value', "8")
+        cy.get('#cardExpYear').should('have.value', "2025")
+        cy.get('#cardBillingStreet').should('have.value', "1234 New Address")
+        cy.get('#cardBillingCity').should('have.value', "New City")
+        cy.get('#cardBillingState').should('have.value', "AL")
+        cy.get('#cardBillingZip').should('have.value', "12345")
     });
+
+    it('Logged-out Access', function () {
+        cy.visit('localhost:3000/booking/1')
+        cy.url().should('match', /login/);
+
+        cy.visit('localhost:3000/booking/portal')
+        cy.url().should('match', /login/);
+
+        cy.visit('localhost:3000/portal/admin/manage-showtimes')
+        cy.url().should('match', /login/);
+
+        cy.visit('localhost:3000/portal/admin/movies')
+        cy.url().should('match', /login/);
+
+        cy.visit('localhost:3000/portal/admin/promotions')
+        cy.url().should('match', /login/);
+
+        cy.visit('localhost:3000/profile/edit')
+        cy.url().should('match', /login/);
+    })
+
+    it('Customer Access', function() {
+        cy.visit('http://localhost:3000')
+        cy.get('#login').click();
+        cy.get('[name="email"]').click();
+        cy.get('[name="email"]').type('dbarfield4@gmail.com');
+        cy.get('[name="password"]').click();
+        cy.get('[name="password"]').type('1234');
+        cy.get('[name="rememberMe"]').check();
+        cy.get('[data-testid="loginFormSubmit"]').click();
+        cy.wait(1000);
+        cy.get('#logout').should('have.text', 'Logout')
+
+        cy.visit('localhost:3000/portal/admin/manage-showtimes')
+        cy.url().should('eq', 'localhost:3000');
+
+        cy.visit('localhost:3000/portal/admin/movies')
+        cy.url().should('eq', 'localhost:3000');
+
+        cy.visit('localhost:3000/portal/admin/promotions')
+        cy.url().should('eq', 'localhost:3000');
+
+        cy.visit('localhost:3000/profile/edit')
+        cy.url().should('eq', 'localhost:3000');
+
+    })
+
 })
--- cinema-frontend/cypress/e2e/ces/addmovie.cy.ts
+++ cinema-frontend/cypress/e2e/ces/addmovie.cy.ts
@@ -65,5 +65,6 @@ describe('template spec', () => {
 
       });
       cy.reload();
+
   })
 })

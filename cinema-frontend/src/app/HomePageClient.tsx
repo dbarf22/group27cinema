711edb28a1fe139bc156bd4bd84dcf88292e2761
dbarf22 <dbarfield4@gmail.com>
2025-12-03 14:09:37 -0500

    Theme change

--- cinema-frontend/src/app/HomePageClient.tsx
+++ cinema-frontend/src/app/HomePageClient.tsx
@@ -17,7 +17,37 @@ export default function HomePageClient({
 
   return (
     <main className="mx-auto max-w-6xl px-4 py-8 space-y-10">
-      {/* Sections that depend on the selected date */}
+
+        <div className="carousel w-full rounded-box">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img
+                    src="opening.jpg"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img
+                    src="nowshowing.jpg"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img
+                    src="promo.jpg"
+                    className="w-full" />
+                <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+
       <MovieSection
         title="Now Playing on: "
         movies={nowPlaying}

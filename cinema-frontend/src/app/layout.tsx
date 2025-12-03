5f629bd799581ae7a3364852c4b7a5672966e434
dbarf22 <dbarfield4@gmail.com>
2025-12-03 13:56:43 -0500

    Theme change

--- cinema-frontend/src/app/layout.tsx
+++ cinema-frontend/src/app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" data-theme={"sunset"}>
+    <html lang="en" data-theme={"cupcake"}>
 
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-base-200 min-h-screen` }>
       <SessionProvider>

b9355c3519599b1b7ab39843f8a518a6a066830c
dbarf22 <dbarfield4@gmail.com>
2025-12-04 21:17:32 -0500

    Don't update empty card number on edit profile

--- cinema_backend/src/main/java/com/group27/cinema_backend/service/UserService.java
+++ cinema_backend/src/main/java/com/group27/cinema_backend/service/UserService.java
@@ -152,7 +152,9 @@ public class UserService {
             for (Card card : updatedUser.getCards()) {
                 int length = card.getCardNumber().length();
                 card.setLastFour(card.getCardNumber().substring(length-4));
-                card.setCardNumber(encoder.encode(card.getCardNumber()));
+                if (card.getCardNumber() != null || card.getCardNumber().isBlank()) {
+                    card.setCardNumber(encoder.encode(card.getCardNumber()));
+                }
                 card.setUser(user);
                 user.getCards().add(card);
             }
